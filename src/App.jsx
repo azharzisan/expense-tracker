0589e2a99220edf5b8235e0002c1769303cbbbfc
Azhar Zisan <azharzisanxtr@gmail.com>
2026-04-02 21:26:39 +0100

    disclaimer added about localstirage to users and added a clear cache btn

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,11 @@ import TotalExpenses from "./ui/TotalExpenses";
 import TableView from "./ui/TableView";
 
 function App() {
+  const handleClear = () => {
+   const updated = []
+   localStorage.setItem("expenses", JSON.stringify(updated))
+   localStorage.setItem("expenseType", JSON.stringify(updated))
+  }
 
   return (
     <>
@@ -30,6 +35,24 @@ function App() {
       <section className="w-full h-auto p-4">
         <DataSearch />
       </section>
+      <div className="w-full h-auto p-4">
+        <div className="w-full p-2 bg-[#ff758f] text-[#800f2f] flex justify-center items-start flex-col gap-1 rounded-2xl">
+          <p className="italic">
+            *Disclaimer: The expense tracker has no database and it is storing
+            data in your browser cache. If having trouble while browsing,
+            please click "Clear Cache" button below and refresh the page.
+          </p>
+          <p className="italic">
+            *Note that: By clearing cache your data will be deleted.
+          </p>
+          <button onClick={handleClear} className="text-md bg-[#800f2f] text-[#fff0f3] hover:bg-[#fff0f3] hover:text-[#800f2f] cursor-pointer py-1 px-3 rounded-xl">
+            Clear Cache
+          </button>
+          <p className="text-[#590d22]">
+            Custom database will be added soon. Stay updated!
+          </p>
+        </div>
+      </div>
     </>
   );
 }
